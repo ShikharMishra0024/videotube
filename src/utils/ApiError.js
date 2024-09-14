1442da22fdc8f30fb2ead1eebf663f1fa7af38cf
unknown <shikharmishra6350650@gmail.com>
2024-09-14 17:22:03 +0530

    setup complete

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,15 +3,15 @@ class ApiError extends Error {
         statusCode,
         message = "something went wrong",
         error =[],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data = null
         this.success = false;
         this.errors = error
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
